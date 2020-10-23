557fd5cfe20f9fe7ea3d98f0cf4f1c23f0c60981
anthonylimo <anthonylimo90@gmail.com>
2020-10-23 15:20:16 +0300

    Fixing structure

--- app.js
+++ app.js
@@ -8,33 +8,38 @@
 
 const credentials = {
     username: 'sandbox',
-    apiKey: ''
+    apiKey: '803f4dd8d184fc18a728616b98fc28ca9e629b9858ca362097bbd274c7dc66aa'
 };
 
 // 2. Using the Africa's Talking package, you can pass your
 // credentials in and initialize the instance
 
-const africastalking = require('africastalking')(credentials);
+const Africastalking = require('africastalking')(credentials);
 
 // 3. Since we are looking to use the Africa's Talking SMS service
 // we will specify this by calling the SMS method from the AT library
 // and assign it to the sms variable
 
-sms = africastalking.SMS;
+sms = Africastalking.SMS;
 
 // 4. The SMS method takes several parameters
 // to - defines which number the SMS should be sent to
-// from - the alphanumeric from AT that names the sender. By default,
+// from - the alphanumeric from AT that names the sender. Wel'll
+// put all these in an options object. By default,
 // without this parameter, we send it with 'AFRICASTKNG'
 // message - this contains the string that you want to send out
 
-response = sms.send(to, from, message);
+const options = {
+    to: ['+25411001002'], //Ensure this is an array
+    message: 'Hello, AT Ninja',
+    from: 'TICTAC-SHOP'
+}
 
 // 5. Next, we print out the response from the Africa's talking 
 // service. You can find the structure of the payload in the API
 // reference here: 
 
-console.log(response);
+sms.send(options).then(console.log(response)).catch(console.log(response));
 
 
 
